b965bc754afc403790cadbdd42a808091d4b7981
Vishal <dewani.vishal206@gmail.com>
2022-12-15 20:11:30 +0500

    copy btn added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const quoteContainer = document.querySelector('.quote');
 const nextBtn = document.querySelector('#next');
 const author = document.querySelector('#author');
+const copyBtn = document.querySelector('#copy');
 const option = {
   method: 'GET',
   url: 'https://quotes15.p.rapidapi.com/quotes/random/',
@@ -23,4 +24,8 @@ nextBtn.addEventListener('click', () => {
       quoteContainer.innerHTML = data.content;
       author.innerHTML = data.originator.name;
     });
+});
+copyBtn.addEventListener('click', () => {
+  let copy = quoteContainer.textContent;
+  navigator.clipboard.writeText(copy);
 });
